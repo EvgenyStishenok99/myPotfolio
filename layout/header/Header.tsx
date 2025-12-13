57096598b04594f8b9f03bfee99e88dbcaaea342
EvgenyStishenok99 <stishenok999@mail.ru>
2025-12-13 18:13:37 +0300

    first commit

---
+++ layout/header/Header.tsx
@@ -0,0 +1,35 @@
+import React from 'react';
+
+export const Header = () => {
+  return (
+    <header>
+      <a href="">
+        <img
+          src=""
+          alt=""
+        />
+      </a>
+
+      <nav>
+        <ul>
+          <li>
+            <a href="">Home</a>
+          </li>
+          <li>
+            <a href="">About</a>
+          </li>
+          <li>
+            <a href="">Tech Stack</a>
+          </li>
+          <li>
+            <a href="">Projects</a>
+          </li>
+          <li></li>
+          <li></li>
+          <li></li>
+        </ul>
+      </nav>
+    </header>
+  );
+};
+
