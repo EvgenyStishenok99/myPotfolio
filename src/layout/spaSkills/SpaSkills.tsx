86f135b788bef944e8b2e4fbb334357cb6a9677c
EvgenyStishenok99 <stishenok999@mail.ru>
2025-12-23 14:09:32 +0300

    add drct for svg in spaSkills

--- src/layout/spaSkills/SpaSkills.tsx
+++ src/layout/spaSkills/SpaSkills.tsx
@@ -7,10 +7,10 @@ import {
 export const SpaSkills = () => {
   return (
     <SpaSkillsStyle id="SpaSkills">
-<div className="topBlock">
-  <h1 className="mainH1">My Tech Stack</h1>
-    <p className="mainP"> Technologies I’ve been working with recently</p>
-</div>
+<TopBlock>
+  <MainH1>My Tech Stack</MainH1>
+    <MainP> Technologies I’ve been working with recently</MainP>
+</TopBlock>
      <BottomBlockWithSkills/>
     </SpaSkillsStyle>
   );
@@ -18,24 +18,23 @@ export const SpaSkills = () => {
 
 const SpaSkillsStyle=styled.div`
   padding-top: 201px  ;
-  
-  .topBlock{
-    margin-bottom: 124px;
-  }
-  .mainH1{
-    font-family: "Poppins", sans-serif;
-    font-weight: 700;
-    font-size: 48px;
-    line-height: 54%;
-    text-align: center;
-    color: #42446e;
-  }
-  .mainP{
-    font-family: "Poppins", sans-serif;;
-    font-weight: 400;
-    font-size: 32px;
-    line-height: 81%;
-    text-align: center;
-    color: #666;
-  }
+`
+const TopBlock = styled.div`
+  margin-bottom: 124px;
+`
+const   MainH1 = styled.h1`
+  font-family: "Poppins", sans-serif;
+  font-weight: 700;
+  font-size: 48px;
+  line-height: 54%;
+  text-align: center;
+  color: #42446e;
+`
+const MainP = styled.p`
+  font-family: "Poppins", sans-serif;;
+  font-weight: 400;
+  font-size: 32px;
+  line-height: 81%;
+  text-align: center;
+  color: #666;
 `
